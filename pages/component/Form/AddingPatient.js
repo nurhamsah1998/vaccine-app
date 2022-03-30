b90bd06db61c9b61cdc208e1e48bdfc25ff13470
nurhamsah <nur.hamsah.cash@gmail.com>
2022-03-30 15:40:49 +0700

    adding dasborad

--- pages/component/Form/AddingPatient.js
+++ pages/component/Form/AddingPatient.js
@@ -1,62 +1,60 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import { Grid } from "@mui/material";
-import { Button } from "@mui/material";
-import CircularProgress from "@mui/material/CircularProgress";
-import TextField from "@mui/material/TextField";
-import SelectInut from "../SelectInput";
-import { useState } from "react";
-import { notification } from "antd";
-import "antd/dist/antd.css";
-import LoadingButton from "@mui/lab/LoadingButton";
-import axios from "axios";
-import { uid } from "uid";
-const moment = require("moment");
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
+import { Button } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import TextField from '@mui/material/TextField';
+import SelectInut from '../SelectInput';
+import { useState } from 'react';
+import { notification } from 'antd';
+import 'antd/dist/antd.css';
+import LoadingButton from '@mui/lab/LoadingButton';
+import axios from 'axios';
+import { uid } from 'uid';
+const moment = require('moment');
 
 export default function BasicTextFields() {
   const [loading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [dad_name, setDad_name] = useState("");
-  const [mom_name, setMom_name] = useState("");
-  const [matematika, setMatematika] = useState("");
-  const [kejuruan, setKejuruan] = useState("");
-  const [bahasa_indonesia, setBahasa_indonesia] = useState("");
-  const [pendidikan_agama, setPendidikan_agama] = useState("");
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [dad_name, setDad_name] = useState('');
+  const [mom_name, setMom_name] = useState('');
+  const [matematika, setMatematika] = useState('');
+  const [kejuruan, setKejuruan] = useState('');
+  const [bahasa_indonesia, setBahasa_indonesia] = useState('');
+  const [pendidikan_agama, setPendidikan_agama] = useState('');
   const openNotification = (placement) => {
     notification.success({
       message: `Sukses Mengirim Data ke Server `,
-      description:
-        "Data sudah tersimpan di server pusat. Lihat di tab list untuk melihat data user lebih lengkap.",
+      description: 'Data sudah tersimpan di server pusat. Lihat di tab list untuk melihat data user lebih lengkap.',
       placement,
     });
   };
   const openNotification1 = (placement, msg) => {
     notification.error({
       message: msg,
-      description: "Nomor yang anda masukkan sudah terdaftar/terverifikasi.",
+      description: 'Nomor yang anda masukkan sudah terdaftar/terverifikasi.',
       placement,
     });
   };
   const openNotification2 = (placement) => {
     notification.error({
       message: `Tidak boleh ada yang kosong`,
-      description:
-        "Tolong masukkan input dengan benar. Tidak memasukan data keinput system tidak akan mengirim data.",
+      description: 'Tolong masukkan input dengan benar. Tidak memasukan data keinput system tidak akan mengirim data.',
       placement,
     });
   };
   const openNotification3 = (placement) => {
     notification.error({
       message: `Nomor telpon tidak VALID`,
-      description: "Tolong masukkan nomor telfon yang valid. contoh : 081234567890",
+      description: 'Tolong masukkan nomor telfon yang valid. contoh : 081234567890',
       placement,
     });
   };
   function nurhamsah(e) {
     e.preventDefault();
-    var waktu = moment().format("MMMM Do YYYY, h:mm:ss a");
+    var waktu = moment().format('MMMM Do YYYY, h:mm:ss a');
     setLoading(true);
     const body = {
       id: uid(16),
@@ -71,33 +69,33 @@ export default function BasicTextFields() {
       bahasa_indonesia: bahasa_indonesia || null,
       pendidikan_agama: pendidikan_agama || null,
     };
-    if (name == "" || phone == "" || address == "" || dad_name == "" || mom_name == "") {
-      openNotification2("bottomRight");
+    if (name == '' || phone == '' || address == '' || dad_name == '' || mom_name == '') {
+      openNotification2('bottomRight');
       setLoading(false);
       return false;
     } else if (phone.length >= 13 || phone.length <= 9) {
-      openNotification3("bottomRight");
+      openNotification3('bottomRight');
       setLoading(false);
       return false;
     }
     axios
-      .post("http://localhost:8000/post", body)
+      .post('http://localhost:8000/post', body)
       .then((res) => {
         console.log(res);
         setLoading(false);
-        openNotification("bottomRight");
-        setName("");
-        setPhone("");
-        setAddress("");
-        setDad_name("");
-        setMom_name("");
+        openNotification('bottomRight');
+        setName('');
+        setPhone('');
+        setAddress('');
+        setDad_name('');
+        setMom_name('');
       })
       .catch(function (error) {
         if (error.response) {
-          openNotification1("bottomRight", error.response.data);
+          openNotification1('bottomRight', error.response.data);
           setLoading(false);
           setName(name);
-          setPhone(error.response.data ? "" : phone);
+          setPhone(error.response.data ? '' : phone);
           setAddress(address);
           setDad_name(dad_name);
           setMom_name(mom_name);
@@ -108,34 +106,21 @@ export default function BasicTextFields() {
     return (
       <div
         style={{
-          display: "flex",
-          alignItems: "start",
-          marginTop: "16px",
+          display: 'flex',
+          alignItems: 'start',
+          marginTop: '16px',
         }}
       >
-        <CircularProgress color="secondary" size={"20px"} sx={{ marginTop: "1px" }} />
-        <p style={{ marginLeft: "20px" }}>Mengirim . .</p>
+        <CircularProgress color="secondary" size={'20px'} sx={{ marginTop: '1px' }} />
+        <p style={{ marginLeft: '20px' }}>Mengirim . .</p>
       </div>
     );
   }
 
   return (
-    <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "space-around" }}>
-      <Box
-        component="form"
-        onSubmit={nurhamsah}
-        sx={{ display: "grid", minWidth: "500px" }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-basic"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          label="Nama"
-          variant="outlined"
-          color="secondary"
-        />
+    <Box sx={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'space-around' }}>
+      <Box component="form" onSubmit={nurhamsah} sx={{ display: 'grid', minWidth: '500px' }} noValidate autoComplete="off">
+        <TextField id="outlined-basic" value={name} onChange={(e) => setName(e.target.value)} label="Nama" variant="outlined" color="secondary" />
         <TextField
           id="outlined-basic"
           value={phone}
@@ -144,33 +129,19 @@ export default function BasicTextFields() {
           type="number"
           variant="outlined"
           color="secondary"
-          sx={{ margin: "20px 0px" }}
+          sx={{ margin: '20px 0px' }}
         />
-        <TextField
-          id="outlined-basic"
-          label="Alamat"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          variant="outlined"
-          color="secondary"
-        />
+        <TextField id="outlined-basic" label="Alamat" value={address} onChange={(e) => setAddress(e.target.value)} variant="outlined" color="secondary" />
         <TextField
           onChange={(e) => setDad_name(e.target.value)}
           id="outlined-basic"
           value={dad_name}
-          sx={{ margin: "20px 0px" }}
+          sx={{ margin: '20px 0px' }}
           label="Nama Ayah"
           variant="outlined"
           color="secondary"
         />
-        <TextField
-          onChange={(e) => setMom_name(e.target.value)}
-          id="outlined-basic"
-          value={mom_name}
-          label="Nama Ibu"
-          variant="outlined"
-          color="secondary"
-        />
+        <TextField onChange={(e) => setMom_name(e.target.value)} id="outlined-basic" value={mom_name} label="Nama Ibu" variant="outlined" color="secondary" />
         <LoadingButton
           // onClick={handleClick}
           sx={{ mt: 2, p: 2 }}
@@ -184,9 +155,9 @@ export default function BasicTextFields() {
           Kirim Data
         </LoadingButton>
       </Box>
-      <Box sx={{ transform: "scale(0.6)", mt: "-130px" }}>
+      {/* <Box sx={{ transform: "scale(0.6)", mt: "-130px" }}>
         <img src="/svg.svg" />
-      </Box>
+      </Box> */}
     </Box>
   );
 }
--- pages/dasboard.js
+++ pages/dasboard.js
@@ -1,25 +1,120 @@
-import { Box, Grid, Typography } from "@mui/material";
-import React from "react";
-import Drawer from "./component/Drawer";
-import { green, indigo, blue } from "@mui/material/colors";
+import { Box, Grid, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import Drawer from './component/Drawer';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { green, indigo, blue, deepOrange, deepPurple, cyan } from '@mui/material/colors';
 
 function test() {
+  const { data, isLoading } = useQuery('get', () => {
+    return axios.get('http://localhost:8000/get');
+  });
+  const userGender = data?.data?.map((e) => {
+    return e.gender;
+  });
+  const femaleGender = userGender?.filter((e) => {
+    return e == 'Perempuan';
+  });
+  const maleGender = userGender?.filter((e) => {
+    return e == 'Laki-Laki';
+  });
   return (
     <Drawer label="DASBOARD">
       <Grid gap={4} container direction="row" justifyContent="center" alignItems="flex-start">
         <Grid
           sx={{
-            bgcolor: green[500],
+            bgcolor: indigo[500],
             height: 230,
             width: 430,
-            borderRadius: "12px",
-            boxShadow: 5,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: indigo[600],
+            },
           }}
           item
         >
-          <Box sx={{ p: 3, position: "relative", height: "100%" }}>
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
             <Typography fontSize={23} fontWeight={700} color="white">
-              Detail Siswa
+              Detail Sekolah
+            </Typography>
+            <Box display="flex" sx={{ mt: 2 }} justifyContent="space-between" color="white">
+              <Box>
+                <Typography>Jumlah Siswa Laki - Laki</Typography>
+                <Typography>Jumlah Siswa Perempuan</Typography>
+              </Box>
+              <Box>
+                <Typography>{maleGender?.length} Siswa</Typography>
+                <Typography>{femaleGender?.length} Siswa</Typography>
+              </Box>
+            </Box>
+            <Box sx={{ position: 'absolute', bottom: '0px', right: '0px', p: 2 }}>
+              <Typography fontSize={23} color="white">
+                856 Total Siswa
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          sx={{
+            bgcolor: blue[500],
+            height: 230,
+            width: 430,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: blue[600],
+            },
+          }}
+          item
+        >
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
+            <Typography fontSize={23} fontWeight={700} color="white">
+              Siswa Terbaik
+            </Typography>
+            <Typography fontSize={15} fontWeight={400} marginTop="-5px" color="white">
+              Hasil berdasarkan total nilai danem terbaik.
+            </Typography>
+            <Box display="flex" sx={{ mt: 1 }} justifyContent="space-between" color="white">
+              <Box>
+                <Typography>Nurhamsh</Typography>
+                <Typography>Agung Sasmitro</Typography>
+                <Typography>Nadya Dwi Arini</Typography>
+                <Typography>Jaka Erlangga Saputra</Typography>
+                <Typography>Laili Febriani</Typography>
+              </Box>
+              <Box>
+                <Typography>(32.2)</Typography>
+                <Typography>(32.1)</Typography>
+                <Typography>(28.6)</Typography>
+                <Typography>(28.2)</Typography>
+                <Typography>(28.0)</Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          sx={{
+            bgcolor: green[500],
+            height: 230,
+            width: 430,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: green[600],
+            },
+          }}
+          item
+        >
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
+            <Typography fontSize={23} fontWeight={700} color="white">
+              Dana BOSS
             </Typography>
             <Box display="flex" sx={{ mt: 2 }} justifyContent="space-between" color="white">
               <Box>
@@ -31,21 +126,121 @@ function test() {
                 <Typography>230 Siswa</Typography>
               </Box>
             </Box>
-            <Box sx={{ position: "absolute", bottom: "0px", right: "0px", p: 3 }}>
+            <Box sx={{ position: 'absolute', bottom: '0px', right: '0px', p: 2 }}>
+              <Typography fontSize={23} color="white">
+                Rp5.000.789.000
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          sx={{
+            bgcolor: deepOrange[500],
+            height: 230,
+            width: 430,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: deepOrange[600],
+            },
+          }}
+          item
+        >
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
+            <Typography fontSize={23} fontWeight={700} color="white">
+              Pengeluaran
+            </Typography>
+            <Box display="flex" sx={{ mt: 2 }} justifyContent="space-between" color="white">
+              <Box>
+                <Typography>Jumlah Siswa Laki - Laki</Typography>
+                <Typography>Jumlah Siswa Perempuan</Typography>
+              </Box>
+              <Box>
+                <Typography>200 Siswa</Typography>
+                <Typography>230 Siswa</Typography>
+              </Box>
+            </Box>
+            <Box sx={{ position: 'absolute', bottom: '0px', right: '0px', p: 2 }}>
+              <Typography fontSize={23} color="white">
+                Rp3.600.000
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          sx={{
+            bgcolor: deepPurple[500],
+            height: 230,
+            width: 430,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: deepPurple[600],
+            },
+          }}
+          item
+        >
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
+            <Typography fontSize={23} fontWeight={700} color="white">
+              Tamu
+            </Typography>
+            <Box display="flex" sx={{ mt: 2 }} justifyContent="space-between" color="white">
+              <Box>
+                <Typography>Jumlah Siswa Laki - Laki</Typography>
+                <Typography>Jumlah Siswa Perempuan</Typography>
+              </Box>
+              <Box>
+                <Typography>200 Siswa</Typography>
+                <Typography>230 Siswa</Typography>
+              </Box>
+            </Box>
+            {/* <Box sx={{ position: 'absolute', bottom: '0px', right: '0px', p: 2 }}>
+              <Typography fontSize={23} color="white">
+                856 Total Siswa
+              </Typography>
+            </Box> */}
+          </Box>
+        </Grid>
+        <Grid
+          sx={{
+            bgcolor: cyan[500],
+            height: 230,
+            width: 430,
+            borderRadius: '12px',
+            transition: 'all 0.4s',
+            '&:hover': {
+              boxShadow: 5,
+              transform: 'translateY(-10px)',
+              bgcolor: cyan[600],
+            },
+          }}
+          item
+        >
+          <Box sx={{ p: 2, position: 'relative', height: '100%' }}>
+            <Typography fontSize={23} fontWeight={700} color="white">
+              Acara Sekolah
+            </Typography>
+            <Box display="flex" sx={{ mt: 2 }} justifyContent="space-between" color="white">
+              <Box>
+                <Typography>Jumlah Siswa Laki - Laki</Typography>
+                <Typography>Jumlah Siswa Perempuan</Typography>
+              </Box>
+              <Box>
+                <Typography>200 Siswa</Typography>
+                <Typography>230 Siswa</Typography>
+              </Box>
+            </Box>
+            <Box sx={{ position: 'absolute', bottom: '0px', right: '0px', p: 2 }}>
               <Typography fontSize={23} color="white">
                 856 Total Siswa
               </Typography>
             </Box>
           </Box>
         </Grid>
-        <Grid
-          sx={{ bgcolor: indigo[500], height: 230, width: 430, borderRadius: "12px" }}
-          item
-        ></Grid>
-        <Grid
-          sx={{ bgcolor: blue[500], height: 230, width: 430, borderRadius: "12px" }}
-          item
-        ></Grid>
       </Grid>
     </Drawer>
   );
